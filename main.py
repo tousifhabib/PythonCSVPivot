e7d637bec4c898f3e49dba0544f5873072d31ee6
Tousif Habib <ts-tousif.habib@rakuten.com>
2024-01-10 10:28:01 +0900

    SAVE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from dataProcessing import load_data, preprocess_data, calculate_totals
-from pdfGeneration import save_pdf, dynamic_columns_for_pdf
+from pdfGeneration import save_pdf, dynamic_columns_for_pdf, save_excel
 
 
 def load_config():
@@ -19,8 +19,15 @@ def create_pdf_file_path(csv_file_path):
     return os.path.join(base_dir, f"{timestamp}_PivotTable.pdf")
 
 
+def create_excel_file_path(csv_file_path):
+    """Generates the file path for the Excel file."""
+    base_dir = os.path.dirname(csv_file_path)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    return os.path.join(base_dir, f"{timestamp}_PivotTable.xlsx")
+
+
 def main(csv_file_path, filters, group_cols, agg_func, agg_col, subtotal_col=None, page_size=letter):
-    """Main function to process data and generate PDF."""
+    """Main function to process data and generate reports."""
     config = load_config()
     try:
         df = load_data(csv_file_path)
@@ -35,17 +42,20 @@ def main(csv_file_path, filters, group_cols, agg_func, agg_col, subtotal_col=Non
         pdf_file_path = create_pdf_file_path(csv_file_path)
         save_pdf(final_data, pdf_file_path, page_size, dynamic_columns, config)
 
+        excel_file_path = create_excel_file_path(csv_file_path)
+        save_excel(final_data, excel_file_path, dynamic_columns, config)
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    csv_file_path = "/Users/ts-tousif.habib/Downloads/TEST/full_report_231220_1926 1.csv"
+    csv_file_path = "/Users/ts-tousif.habib/Development/Repos/automatic-failure-verification/csv_export/full_report_240110_0946.csv"
     filters = {"result": "failed"}
-    group_cols = ["errorType", "comment", "feature"]
+    group_cols = ["errorType", "comment"]
     agg_func = "count"
     agg_col = "Count"
-    subtotal_col = ["errorType", "comment"]
+    subtotal_col = ["errorType"]
 
     custom_page_size = (10 * inch, 20 * inch)
 
--- pdfGeneration.py
+++ pdfGeneration.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from reportlab.platypus import SimpleDocTemplate, Table
 import logging
 from tableStyling import create_table_data, apply_table_styles
@@ -16,6 +17,8 @@ def save_pdf(data, file_path, page_size, dynamic_cols, config):
         table_data = create_table_data(data, dynamic_cols)
         table = Table(table_data)
 
+        print(table_data)
+
         apply_table_styles(table, table_data, dynamic_cols, config)
         doc.build([table])
         logging.info("PDF generation completed successfully.")
@@ -23,6 +26,13 @@ def save_pdf(data, file_path, page_size, dynamic_cols, config):
     except Exception as e:
         logging.error(f"Error building PDF: {e}")
 
+
+def save_excel(data, file_path, dynamic_cols, config):
+    """Create the Excel report from the given DataFrame."""
+    with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
+        data.to_excel(writer, sheet_name='Report', index=False)
+
+    print(f"Saved Excel report to {file_path}")
 
 def dynamic_columns_for_pdf(group_cols, agg_col):
     """Determine dynamic columns for the PDF based on grouping and aggregate columns."""
