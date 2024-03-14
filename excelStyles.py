0727e6a137ea85fb7f6e9d1c6c61aedf82ea3163
Tousif Habib <ts-tousif.habib@rakuten.com>
2024-03-14 16:04:11 +0900

    SAVE

--- excelStyles.py
+++ excelStyles.py
@@ -61,13 +61,17 @@ def apply_excel_colors(worksheet: Worksheet, config: Dict[str, Any], df: DataFra
     apply_row_styles(worksheet, config, dynamic_cols)
 
 
-def merge_empty_cells(worksheet: Worksheet, df: pd.DataFrame) -> None:
+import pandas as pd
+from typing import Tuple, Dict, Set
+
+
+def merge_empty_cells(worksheet, df: pd.DataFrame) -> None:
     subtotal_rows, subtotal_levels = identify_subtotal_rows(df)
     merge_first_column(worksheet, subtotal_rows, subtotal_levels)
     merge_remaining_columns(worksheet, df, subtotal_rows, subtotal_levels)
 
 
-def identify_subtotal_rows(df: pd.DataFrame) -> (Set[int], Dict[int, int]):
+def identify_subtotal_rows(df: pd.DataFrame) -> Tuple[Set[int], Dict[int, int]]:
     subtotal_rows = set()
     subtotal_levels = {}
     for col_idx, col in enumerate(df.columns[:-2], start=1):
@@ -76,57 +80,46 @@ def identify_subtotal_rows(df: pd.DataFrame) -> (Set[int], Dict[int, int]):
         for row_idx, value in enumerate(df[col], start=2):
             if pd.isna(value) or value == '':
                 blank_seen = True
-            elif blank_seen:
-                subtotal_rows.add(row_idx)
-                if col_idx == 1:
-                    subtotal_levels[row_idx] = level
-                blank_seen = False
-                level += 1
-            if "Grand Total" in str(value):
-                subtotal_rows.add(row_idx)
-                if col_idx == 1:
-                    subtotal_levels[row_idx] = level
+            else:
+                if blank_seen or "Grand Total" in str(value):
+                    subtotal_rows.add(row_idx)
+                    if col_idx == 1:
+                        subtotal_levels[row_idx] = level
+                    blank_seen = False
+                    level += 1
     return subtotal_rows, subtotal_levels
 
 
-def merge_first_column(worksheet: Worksheet, subtotal_rows: Set[int], subtotal_levels: Dict[int, int]) -> None:
+def merge_first_column(worksheet, subtotal_rows: Set[int], subtotal_levels: Dict[int, int]) -> None:
+    row_idx_list = sorted(subtotal_rows)
     start_row = 3
     prev_level = -1
-    for row_idx in sorted(subtotal_rows):
+    for row_idx in row_idx_list:
         level = subtotal_levels.get(row_idx, -1)
-        if level > prev_level:
-            if row_idx > start_row:
-                merge_cells(worksheet, start_row, 1, row_idx - 1)
+        if level > prev_level and row_idx > start_row:
+            merge_cells(worksheet, start_row, 1, row_idx - 1)
             start_row = row_idx + 1
         prev_level = level
+    if start_row <= row_idx_list[-1]:
+        merge_cells(worksheet, start_row, 1, row_idx_list[-1])
 
-    if start_row <= row_idx:
-        merge_cells(worksheet, start_row, 1, row_idx)
 
-
-def merge_remaining_columns(worksheet: Worksheet, df: pd.DataFrame, subtotal_rows: Set[int],
+def merge_remaining_columns(worksheet, df: pd.DataFrame, subtotal_rows: Set[int],
                             subtotal_levels: Dict[int, int]) -> None:
     for col_idx, col in enumerate(df.columns, start=1):
         start_row = None
         for row_idx, value in enumerate(df[col], start=2):
             is_subtotal_row = row_idx in subtotal_rows
-
-            if is_subtotal_row:
-                if start_row is not None and (row_idx - 1) != start_row:
-                    merge_cells(worksheet, start_row, col_idx, row_idx - 1)
-                start_row = row_idx + 1
-            elif pd.isna(value) or value == '':
-                if start_row is None:
-                    start_row = row_idx
-            else:
-                if start_row is not None and col_idx > subtotal_levels.get(row_idx, 0) + 1:
+            if is_subtotal_row or not pd.isna(value) and value != '':
+                if start_row is not None and row_idx - 1 != start_row and col_idx > subtotal_levels.get(row_idx, 0) + 1:
                     merge_cells(worksheet, start_row, col_idx, row_idx - 1)
                 start_row = None
+            elif start_row is None:
+                start_row = row_idx
+        if start_row is not None and start_row <= len(df):
+            merge_cells(worksheet, start_row, col_idx, len(df) + 1)
 
-        if start_row is not None and start_row <= row_idx:
-            merge_cells(worksheet, start_row, col_idx, row_idx)
 
-
-def merge_cells(worksheet: Worksheet, start_row: int, col_idx: int, end_row: int) -> None:
+def merge_cells(worksheet, start_row: int, col_idx: int, end_row: int) -> None:
     if end_row > start_row:
         worksheet.merge_cells(start_row=start_row, start_column=col_idx, end_row=end_row, end_column=col_idx)
