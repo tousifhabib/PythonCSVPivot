f20fee0d3e5fae4572657152ac2f0680c1cc5d7f
Tousif Habib <ts-tousif.habib@rakuten.com>
2024-03-22 15:50:23 +0900

    SAVE

--- output_generation/excel/excelStyles.py
+++ output_generation/excel/excelStyles.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, Any, List, Tuple, Set
 import pandas as pd
 from openpyxl.styles import PatternFill, Font
@@ -6,6 +7,8 @@ from openpyxl.worksheet.worksheet import Worksheet
 from reportlab.lib import units
 from reportlab.lib.pagesizes import letter
 
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def calculate_dynamic_page_size(table_width, table_height, config, paginate, num_rows):
     default_cell_height = 0.25 * units.inch
@@ -86,77 +89,46 @@ def apply_excel_colors(worksheet: Worksheet, config: Dict[str, Any]) -> None:
 
 
 def merge_empty_cells(worksheet, df: pd.DataFrame) -> None:
-    subtotal_rows, subtotal_levels = identify_subtotal_rows(df)
-    merge_first_column(worksheet, subtotal_rows, subtotal_levels)
-    merge_remaining_columns(worksheet, df, subtotal_rows, subtotal_levels)
+    for col_idx, col in enumerate(df.columns, start=1):
+        merge_column(worksheet, df, col_idx)
 
 
-def identify_subtotal_rows(df: pd.DataFrame) -> Tuple[Set[int], Dict[int, int]]:
-    subtotal_rows = set()
-    subtotal_levels = {}
-    for col_idx, col in enumerate(df.columns[:-2], start=1):
-        blank_seen = False
-        level = 0
-        for row_idx, value in enumerate(df[col], start=2):
-            if pd.isna(value) or value == '':
-                blank_seen = True
-            else:
-                if blank_seen or "Grand Total" in str(value):
-                    subtotal_rows.add(row_idx)
-                    if col_idx == 1:
-                        subtotal_levels[row_idx] = level
-                    blank_seen = False
-                    level += 1
-    return subtotal_rows, subtotal_levels
+def merge_column(worksheet, df: pd.DataFrame, col_idx: int) -> None:
+    start_row = None
+    prev_subtotal_row = None
+    for row_idx, value in enumerate(df.iloc[:, col_idx - 1], start=2):
+        if is_special_row1(df, row_idx):
+            if start_row is not None and start_row < row_idx - 1:
+                merge_cells(worksheet, col_idx, start_row, row_idx - 1)
+            start_row = None
+            prev_subtotal_row = row_idx
+        elif should_span(value):
+            if prev_subtotal_row is None or row_idx > prev_subtotal_row:
+                start_row = start_row if start_row is not None else row_idx
+        else:
+            if start_row is not None and start_row < row_idx:
+                merge_cells(worksheet, col_idx, start_row, row_idx)
+            start_row = None
+    if start_row is not None and start_row < len(df) + 2:
+        merge_cells(worksheet, col_idx, start_row, len(df) + 2)
 
 
-def merge_first_column(worksheet, subtotal_rows: Set[int], subtotal_levels: Dict[int, int]) -> None:
-    row_idx_list = sorted(subtotal_rows)
-    start_row = 3
-    prev_level = -1
-    for row_idx in row_idx_list:
-        level = subtotal_levels.get(row_idx, -1)
-        if level > prev_level and row_idx > start_row:
-            merge_cells(worksheet, start_row, 1, row_idx - 1)
-            start_row = row_idx + 1
-        prev_level = level
-    if start_row <= row_idx_list[-1]:
-        merge_cells(worksheet, start_row, 1, row_idx_list[-1])
+def is_special_row1(df: pd.DataFrame, row_idx: int) -> bool:
+    row_data = df.iloc[row_idx - 2]
+    is_total = "Grand Total" in str(row_data.iloc[0])
+    if row_idx > 2 and not is_total:
+        if not pd.isna(row_data.iloc[0]) and all(pd.isna(val) for val in row_data.iloc[1:-2]):
+            prev_row_data = df.iloc[row_idx - 3]
+            if str(prev_row_data.iloc[0]) != str(row_data.iloc[0]) and not all(
+                    pd.isna(val) for val in prev_row_data.iloc[1:-2]):
+                return True
+    return is_total
 
 
-def merge_remaining_columns(worksheet, df: pd.DataFrame, subtotal_rows: Set[int],
-                            subtotal_levels: Dict[int, int]) -> None:
-    for col_idx, col in enumerate(df.columns[:-2], start=1):
-        start_row = None
-        prev_subtotal_row = None
-        prev_subtotal_level = -1
-        data_row_found = False
-
-        for row_idx, value in enumerate(df[col], start=2):
-            is_subtotal_row = row_idx in subtotal_rows
-            current_subtotal_level = subtotal_levels.get(row_idx, -1)
-
-            if is_subtotal_row:
-                if start_row is not None and row_idx - 1 != start_row and current_subtotal_level > prev_subtotal_level:
-                    merge_cells(worksheet, start_row, col_idx, row_idx - 1)
-                elif prev_subtotal_row is not None and current_subtotal_level == prev_subtotal_level and not data_row_found:
-                    merge_cells(worksheet, prev_subtotal_row + 1, col_idx, row_idx - 1)
-                start_row = None
-                prev_subtotal_row = row_idx
-                prev_subtotal_level = current_subtotal_level
-                data_row_found = False
-            elif not pd.isna(value) and value != '':
-                if start_row is not None:
-                    merge_cells(worksheet, start_row, col_idx, row_idx - 1)
-                start_row = None
-                data_row_found = True
-            elif start_row is None:
-                start_row = row_idx
-
-        if start_row is not None and start_row <= len(df):
-            merge_cells(worksheet, start_row, col_idx, len(df) + 1)
+def should_span(cell_value) -> bool:
+    return pd.isna(cell_value) or cell_value == ""
 
 
-def merge_cells(worksheet, start_row: int, col_idx: int, end_row: int) -> None:
-    if end_row > start_row:
-        worksheet.merge_cells(start_row=start_row, start_column=col_idx, end_row=end_row, end_column=col_idx)
+def merge_cells(worksheet, col_idx: int, start_row: int, end_row: int) -> None:
+    if start_row is not None and end_row > start_row:
+        worksheet.merge_cells(start_row=start_row, start_column=col_idx, end_row=end_row - 1, end_column=col_idx)
